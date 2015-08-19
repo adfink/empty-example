c2267151b876cad4c8990b7a4db7fb53b833abeb
Andrew Fink <andrewfink8@gmail.com>
2015-08-19 14:14:57 -0600

    trying out p5

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  createCanvas(windowWidth, windowHeight);
+
+  //createCanvas(640,480);
+
+  translate(580, 200);
+  //noStroke();
+  for (var i = 0; i < 20; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/10);
+  }
+}
+
+function draw() {
+  // draw stuff here
+
+  ellipse(50, 50, 50, 50)
+}
